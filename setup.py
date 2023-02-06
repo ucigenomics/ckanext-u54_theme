30551713d1a5fc3ba703319ded6874b276287da9
rarsenal <iychang@uci.edu>
2023-02-06 11:43:49 -0800

    Update setup.py
    added info support for python 3.8 and 3.10

--- setup.py
+++ setup.py
@@ -44,6 +44,9 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
     ],
 
 
